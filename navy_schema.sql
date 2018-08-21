6c292e9e95a81785a337adf21b9065c7c59c64d9
Kyle Tycholiz <tycholiz22@hotmail.com>
2018-08-21 03:13:59 +0000

    add gitignore file

--- navy_schema.sql
+++ navy_schema.sql
@@ -1,1 +1,26 @@
-asda
+DROP TABLE fleet, ship, sailors, assignment, rank;
+
+CREATE TABLE fleet (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(50)
+);
+CREATE TABLE ship (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(50),
+    date_built SMALLINT
+);
+CREATE TABLE sailors (
+    id INTEGER PRIMARY KEY,
+    date_of_birth SMALLINT,
+    name VARCHAR(50)
+);
+CREATE TABLE assignment (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(50),
+    start_date SMALLINT,
+    end_date SMALLINT
+);
+CREATE TABLE rank (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(50)
+);
