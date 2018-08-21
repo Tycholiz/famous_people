1f73e6948520a3864bf20309e2ab0f05f119643e
Kyle Tycholiz <tycholiz22@hotmail.com>
2018-08-21 21:30:44 +0000

    set up migrate

---
+++ knexfile.js
@@ -0,0 +1,35 @@
+// Update with your config settings.
+const settings = require("./settings");
+module.exports = {
+  //development, staging and production are known as 'environments'
+  
+  development: {
+    client: 'pg',
+    connection: settings
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: settings,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: settings,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
